88940e0b5223601f6973576c7e349150b4268988
jacob-53 <jacob8753@gmail.com>
2025-03-14 17:03:11 +0900

    list2df

---
+++ src/movie/api/call.py
@@ -0,0 +1,33 @@
+import os
+import requests
+import pandas as pd
+
+
+
+BASE_URL = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
+KEY=os.getenv("MOVIE_KEY")
+
+def gen_url(dt="20120101",url_param={}):
+    #key=",".join(url_param.keys())
+    #value=",".join(url_param.values())
+    url = f"{BASE_URL}?key={KEY}&targetDt={dt}"
+    for k,v in url_param.items():
+        url = url + f"&{k}={v}"
+    return url
+
+def call_api(dt="20120101",url_param={}):
+    try:
+        res = requests.get(gen_url(dt,url_param))
+        data = res.json()
+        if res.status_code == 200:
+            a = data["boxOfficeResult"]["dailyBoxOfficeList"]
+            return a
+    except requests.exceptions.RequestException as e:
+        return f"API 요청실패{e}"
+    
+    
+def list2df(data:list, date: str):
+    df= pd.DataFrame(data)
+    df["dt"]=date   
+    return df
+    
---
+++ tests/test_call.py
@@ -0,0 +1,34 @@
+from movie.api.call import gen_url,call_api,list2df
+import os
+import requests
+import pandas as pd
+
+def test_gen_url():
+    r = gen_url()
+    print(r)
+    assert "kobis" in r
+    assert "targetDt" in r
+    assert os.getenv("MOVIE_KEY") in r
+    
+def test_gen_url_default():
+    r = gen_url(url_param={"multiMovieYn":"N","repNationCd":"K"})
+    assert "&multiMovieYn=N" in r
+    assert "&repNationCd=K" in r
+    
+def test_call_api():
+    r= call_api()
+    assert isinstance(r,list)
+    assert isinstance(r[0]['rnum'],str)
+    assert len(r) == 10
+    for i in r:
+        assert isinstance(i,dict)
+        
+def test_list2df():
+    ymd = "20210101"
+    data = call_api(dt=ymd)
+    df = list2df(data,ymd)
+    assert isinstance(df, pd.DataFrame)
+    assert len(data) == len(df)
+    assert set(data[0].keys()).issubset(set(df.columns)) #==> 1,2,3 issubset 1,2,3,4,5 ==> True
+    assert "dt" in df.columns, "dt 컬럼이 있어야 함"
+    assert (df["dt"] == ymd).all(), "모든 컬럼에 입력된 날짜 값이 존재 해야 함"
